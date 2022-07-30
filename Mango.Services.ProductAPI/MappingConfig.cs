e485b8404ae7bc56f071719f7e68e657fda8810b
hussain1998 <hussain-md@outlook.com>
2022-07-30 12:17:55 +0800

    Add AutoMapping between Product and ProductDto

---
+++ Mango.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Mango.Services.ProductAPI.Models;
+using Mango.Services.ProductAPI.Models.Dto;
+
+namespace Mango.Services.ProductAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                // Create mapping between ProductDto and Product. Can also cut short the mapping by using .ReverseMap()
+                // Automatically maps between the 2 classes if they have the same name. Can also state the mappign
+                config.CreateMap<ProductDto, Product>();
+                config.CreateMap<Product, ProductDto>();
+            });
+            return mappingConfig;
+        } 
+    }
+}
