1fd040d00f8c6166184703fe0ed3bde71c46efc2
hussain1998 <hussain-md@outlook.com>
2022-08-22 17:31:23 +0800

    Removed azure sql db secrets and using local user-secrets

--- Mango.Services.ProductAPI/Program.cs
+++ Mango.Services.ProductAPI/Program.cs
@@ -2,13 +2,23 @@ using AutoMapper;
 using Mango.Services.ProductAPI;
 using Mango.Services.ProductAPI.DbContexts;
 using Mango.Services.ProductAPI.Repository;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
-
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// To add user secrets from secrets.json file. (Actually not needed as user secrets are included by default) can type the following command: `dotnet user-secrets init`
+builder.Configuration.AddUserSecrets(Assembly.GetExecutingAssembly(), true); // Pass "true" for optional as if users have not set up the user secret file on their machine yet, the following error will not occur: System.IO.FileNotFoundException: The configuration file 'secrets.json' was not found and is not optional
+
+// Below block of code is to use local user-secrets to store account details. Refer to: https://docs.microsoft.com/en-us/aspnet/core/security/app-secrets?view=aspnetcore-6.0&tabs=windows
+var conStrBuilder = new SqlConnectionStringBuilder(builder.Configuration.GetConnectionString("DefaultConnection"));
+conStrBuilder.UserID = builder.Configuration["userid"];
+conStrBuilder.Password = builder.Configuration["password"];
+//-----------------------------------------------------------
+
 // Add services to the container.
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(conStrBuilder.ConnectionString));
 
 IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
 builder.Services.AddSingleton(mapper);
