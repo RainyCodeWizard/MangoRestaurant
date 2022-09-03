57dc0392a5b7bb2d524e9e6f376b4f281b8d7bb3
hussain1998 <hussain-md@outlook.com>
2022-09-03 10:42:54 +0800

    Create Constants for IdentityResource, ApiScope and Clients

---
+++ Mango.Services.Identity/Constants.cs
@@ -0,0 +1,53 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+
+namespace Mango.Services.Identity
+{
+    public class Constants
+    {
+        public const string Admin = "Admin";
+        public const string Customer = "Customer";
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope> { 
+                new ApiScope(name: "admin", displayName: "Mango Admin"),
+                new ApiScope(name: "read", displayName: "Read Scope"),
+                new ApiScope(name: "write", displayName: "Write Scope"),
+                new ApiScope(name: "delete", displayName: "Delete Scope"),
+            };
+        public static IEnumerable<object> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    ClientSecrets = { new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = {"read", "write", "profile"}
+                },
+                new Client
+                {
+                    ClientId = "mango",
+                    ClientSecrets = { new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris={ "https://localhost:44337/signin-oidc" },
+                    PostLogoutRedirectUris={ "https://localhost:44337/signout-callback-oidc" },
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Email,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "admin"
+                    }
+                }
+            };
+    }
+}
