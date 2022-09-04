dbfb67e9158b6011d9486bb95700409ce54615aa
hussain1998 <hussain-md@outlook.com>
2022-09-04 13:37:58 +0800

    Updated Dependency Injection and done database migration, refer to: https://www.yogihosting.com/aspnet-core-identity-setup/

--- Mango.Services.Identity/Program.cs
+++ Mango.Services.Identity/Program.cs
@@ -1,6 +1,38 @@
+using Mango.Services.Identity;
+using Mango.Services.Identity.DbContext;
+using Mango.Services.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// To add user secrets from secrets.json file. (Actually not needed as user secrets are included by default) can type the following command: `dotnet user-secrets init`
+builder.Configuration.AddUserSecrets(Assembly.GetExecutingAssembly(), true); // Pass "true" for optional as if users have not set up the user secret file on their machine yet, the following error will not occur: System.IO.FileNotFoundException: The configuration file 'secrets.json' was not found and is not optional
+
+// Below block of code is to use local user-secrets to store account details. Refer to: https://docs.microsoft.com/en-us/aspnet/core/security/app-secrets?view=aspnetcore-6.0&tabs=windows
+var conStrBuilder = new SqlConnectionStringBuilder(builder.Configuration.GetConnectionString("DefaultConnection"));
+conStrBuilder.UserID = builder.Configuration["userid"];
+conStrBuilder.Password = builder.Configuration["password"];
+//-----------------------------------------------------------
+
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(conStrBuilder.ConnectionString));
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>() // IdentityRole is default
+    .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.Events.RaiseFailureEvents = true;
+}).AddInMemoryIdentityResources(Constants.IdentityResources)
+.AddInMemoryApiScopes(Constants.ApiScopes)
+.AddInMemoryClients(Constants.Clients)
+.AddAspNetIdentity<ApplicationUser>()
+.AddDeveloperSigningCredential();  //This is for dev only scenarios when you don’t have a certificate to use.
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -17,7 +49,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseIdentityServer();
 app.UseAuthorization();
 
 app.MapControllerRoute(
